5eb1814c3b8331d9da4ddcd99751ad911e05e678
ninobrooks <ninobrooks@gmail.com>
2014-09-04 14:05:46 -0500

    added cmd-docs file (basically a proving ground for commands before they go to Dockerfile)

--- start-rest.sh
+++ start-rest.sh
@@ -1,7 +1,16 @@
+#scratch this. script its creation.
+
 #!/bin/sh
 #Copy of my my dev env start-rest script. Modify for use.
+#ONLY use with non-bundled ripple-rest. Bundled rest can-should
+#be started by gatewayd
 
-cd /opt/ripple-rest
+
+#example: cd /opt/ripple-rest
+cd /path/to/ripple-rest
+
+#hm, apply sed to this as well.
 export DATABASE_URL=postgres://ripple_rest_user:kTzkbwyZwC@localhost:5432/ripple_rest_db
-sudo -E -u restful /usr/bin/node server.js
 
+#if you are root, construct a su command instead
+sudo -E -u restful /usr/bin/node server.js
